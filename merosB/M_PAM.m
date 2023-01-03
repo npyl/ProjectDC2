d5c9efacab68670387d00adb7ed923e7469eb226
npyl <n.pylarinos@hotmail.com>
2023-01-03 17:35:51 +0200

    more of MerosB

---
+++ merosB/M_PAM.m
@@ -0,0 +1,29 @@
+function S_t = M_PAM(symbols, M)
+    Tsymbol = 40;
+
+    Tc = 4;
+    fc = 1/Tc;
+
+    Es = 1;     % gia gT(t), iso me 1 apo ekfwnhsi
+
+    A = 0;      % platos
+    m = symbols + 1;
+
+    % ------------
+    % define gT(t)
+    % ------------
+    t = 1:Tsymbol;  % pedio orismou ths gT opou isxyei o parakatw typos:
+    gT = sqrt(2*Es / Tsymbol);
+
+    % Calculate A (amplitude)
+    for i = 1:M
+        A = A + (2*i + (M + 1)).^2;
+    end
+    A = 1 / sqrt(A/M);
+
+    for i = 1:length(m)
+        Am = (2*m(i) - (M+1))*A;        % Auto antistoixei sto Sm sthn ekfwnisi (Typos (1))
+
+        S_t(i,:) = Am * gT * cos(2 * pi * fc * t);
+    end
+end
--- merosB/askisi2.m
+++ merosB/askisi2.m
@@ -5,8 +5,10 @@ bits = randsrc(10^4, 1, [0, 1]);
 
 % M-PAM
 for i=1:length(M)
-    m = M(i);
+    m = M(i);           % m, bits-PAM
     
-    % s(m)   
-    s = mapper(bits, m);
+    S_m = mapper(bits, m);
+    S_t = M_PAM(S_m, m);
+
+    
 end
