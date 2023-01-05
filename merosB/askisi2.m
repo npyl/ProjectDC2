073f0f9c3bcb3a9d3900ca32fe04e1f57d154b8b
npyl <n.pylarinos@hotmail.com>
2023-01-05 18:17:13 +0200

    draft

--- merosB/askisi2.m
+++ merosB/askisi2.m
@@ -1,14 +1,14 @@
-M = [4, 8];
+M = 4;
+Lb = 10000;
 
 % dyadiki akolouthia
 bits = randsrc(10^4, 1, [0, 1]);
 
 % M-PAM
-for i=1:length(M)
-    m = M(i);           % m, bits-PAM
-    
-    S_m = mapper(bits, m);
-    S_t = M_PAM(S_m, m);
 
-    
+S_m = mapper(bits, M);
+S_t = M_PAM(S_m, M);
+
+for snr = 0:2:20
+    S_final = S_t + noise(snr, M, Lb, size(S_t));
 end
